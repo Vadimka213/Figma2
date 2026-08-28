ccdfaa0786f7f20cf3417adc10b63f1bc0bdde71
Vadimka <andrushchakvadim10@gmail.com>
2026-08-28 08:59:04 -0500

    Fix all

--- mobile-menu.js
+++ mobile-menu.js
@@ -1,17 +1,8 @@
-const menuButton = document.querySelector('.header-menu');
-const header = document.querySelector('.header');
-const menuIcon = document.querySelector('.header-mobile');
+const menuButton = document.querySelector(".header-menu");
+const navigation = document.querySelector(".header-navigation");
+const body = document.body;
 
-menuButton.addEventListener('click', () => {
-    const isOpen = header.classList.toggle('is-open');
-
-    document.body.classList.toggle('no-scroll', isOpen);
-
-    if (isOpen) {
-        menuIcon.src = './images/menu.png';
-        menuIcon.alt = 'Закрити меню';
-    } else {
-        menuIcon.src = './images/close.png';
-        menuIcon.alt = 'Відкрити меню';
-    }
+menuButton.addEventListener("click", () => {
+    navigation.classList.toggle("is-open");
+    body.classList.toggle("menu-open");
 });
